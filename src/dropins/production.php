230fe4892ee2ac0be7d841d101da399edca39c55
amecea <flavius@presslabs.com>
2024-05-08 11:44:20 +0300

    Add more settings set in .env file

--- src/dropins/production.php
+++ src/dropins/production.php
@@ -9,7 +9,7 @@ use function Env\env;
  * No longer needed and causes more problems in HTTP/2 context
  */
 Config::define('CONCATENATE_SCRIPTS', false);
-//TODO: check this
+
 Config::define('WP_CACHE', true);
 
 Config::define('WP_MEMORY_LIMIT', env('WP_MEMORY_LIMIT') ?? '40M');
@@ -17,9 +17,17 @@ Config::define('WP_MAX_MEMORY_LIMIT', env('WP_MAX_MEMORY_LIMIT') ?? '256M');
 
 Config::define('EMPTY_TRASH_DAYS', env('EMPTY_TRASH_DAYS') ?? 30);
 
+// live debugging parameters
 Config::define('WP_DEBUG', env('WP_DEBUG') ?? false);
 Config::define('SAVEQUERIES', env('SAVEQUERIES') ?? false);
 Config::define('SCRIPT_DEBUG', env('SCRIPT_DEBUG') ?? false);
+Config::define('WP_DEBUG_DISPLAY', false);
+Config::define('WP_DEBUG_LOG', false);
+
+Config::define('FS_METHOD', 'direct');
+Config::define('YARPP_CACHE_TYPE', 'postmeta');
+
+Config::define('COOKIE_DOMAIN', env('COOKIE_DOMAIN') ?? '');
 
 Config::define('WP_AUTO_UPDATE_CORE', false);
 
