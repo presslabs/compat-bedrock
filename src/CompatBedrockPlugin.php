646fd3a80ee6fafbb7f3cd0b73535f782fc77e87
amecea <flavius@presslabs.com>
2024-05-08 13:33:57 +0300

    Add WP_CLI comand for setup.

--- src/CompatBedrockPlugin.php
+++ src/CompatBedrockPlugin.php
@@ -1,22 +1,18 @@
 <?php
 namespace Presslabs\CompatBedrock;
 
+use WP_CLI;
 use Presslabs\CompatBedrock\Exceptions\ConstantAlreadyDefinedException;
 use function Env\env;
+use RuntimeException;
 
 
 class CompatBedrockPlugin {
-
-    private function pl_ensure_dropin( $dropin ) {
-        $src = __DIR__ . "/dropins/$dropin";
-        $dest = WP_CONTENT_DIR . "/$dropin";
-
-        $this->ensure_symlink($src, $dest);
-
-        return;
+    function __construct() {
+        WP_CLI::add_command( 'presslabs-setup', [$this, 'cli_configure'] );
     }
 
-    private function ensure_symlink( $src, $dest ) {
+    private function ensure_symlink( string $src, string $dest ) {
         $src_real = realpath( $src );
         if ( false === $src_real ) {
             trigger_error( sprintf( 'Cannot find %s drop-in.', $src ), E_USER_ERROR );
@@ -38,10 +34,44 @@ class CompatBedrockPlugin {
         }
     }
 
-    public function install() {
-        $this->pl_ensure_dropin( 'object-cache.php' );
-        $this->ensure_symlink( '/www/presslabs/dropins/advanced-cache.php', WP_CONTENT_DIR . '/advanced-cache.php' );
+    private function cli_ensure_copyfile( string $src, string $dest ) {
+        $dest = realpath($dest);
 
-        $this->ensure_symlink( __DIR__ . '/dropins/production.php', '/app/config/environments/production.php' );
+        if (file_exists($dest)) {
+            $dest_content_hash = hash_file('sha256', $dest);
+            $src_content_hash = hash_file('sha256', $src);
+            if ($dest_content_hash == $src_content_hash) {
+                return true;
+            }
+
+            WP_CLI::confirm( "Warning: Your environments/production.php exists and differs from presslabs/production.php. Do you want to overwrite?" );
+        }
+
+        return copy( $src, $dest );
+    }
+
+    public function ensure_dropins() {
+        $this->ensure_symlink( __DIR__ . '/dropins/object-cache.php', WP_CONTENT_DIR . '/object-cache.php' );
+        $this->ensure_symlink( '/www/presslabs/dropins/advanced-cache.php', WP_CONTENT_DIR . '/advanced-cache.php' );
+    }
+
+    public function install() {
+        $this->ensure_dropins();
+    }
+
+    public function cli_configure( $args, $assoc_args ) {
+        if (WP_ENV == "production") {
+            throw new RuntimeException("This command should not be ran in a production environment but in development mode.");
+        }
+
+        $success = $this->cli_ensure_copyfile( __DIR__ . '/dropins/production.php', WP_CONTENT_DIR . '/../../config/environments/production.php' );
+
+        if (!$success) {
+            WP_CLI::error( "Failed to copy file!", $exit = true );
+        }
+
+        $this->ensure_dropins();
+
+        WP_CLI::success( "Presslabs environment config is ok!" );
     }
 }
